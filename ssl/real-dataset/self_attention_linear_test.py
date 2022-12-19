05f387e244ef80d15dce91d7aa7d34edb2aeac5a
Yuandong Tian <yuandong.tian@gmail.com>
2022-12-19 09:04:34 -0800

    Can run and get some result

--- ssl/real-dataset/self_attention_linear_test.py
+++ ssl/real-dataset/self_attention_linear_test.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,9 +14,9 @@ log = logging.getLogger(__file__)
 
 
 class LinearModel(nn.Module):
-    def __init__(self, L):
+    def __init__(self, L, num_class):
         super(LinearModel, self).__init__()
-        self.model = nn.Linear(L, 1)
+        self.model = nn.Linear(L, num_class)
         self.L = L
         
     def forward(self, x):
@@ -25,10 +26,10 @@ class LinearModel(nn.Module):
     
 
 class Model(nn.Module):
-    def __init__(self, M, L, d):
+    def __init__(self, M, L, d, num_class):
         super(Model, self).__init__()
         self.M = M
-        self.embed = nn.Embedding(M, d, max_norm=1)
+        self.embed = nn.Embedding(M, d) # max_norm=1)
 
         self.use_WkWq = False
 
@@ -36,8 +37,11 @@ class Model(nn.Module):
             self.Wk = nn.Linear(d, 2*d, bias=False)
             self.Wq = nn.Linear(d, 2*d, bias=False)
 
-        self.V_dim = 1
-        self.V = nn.Embedding(M, self.V_dim)
+        self.V = nn.Embedding(M, d)
+        self.w1 = nn.Linear(d, d)
+        self.w2 = nn.Linear(d, num_class)
+        self.w3 = nn.Linear(num_class * L, num_class)
+        self.relu = nn.ReLU()
         self.d = d
         self.L = L
         
@@ -72,41 +76,153 @@ class Model(nn.Module):
         # V_sel size = [bs, L, V_dim]
         # output size = [bs, L, V_dim]
         output = torch.bmm(attentions, V_sel)
-        return output
+        output = self.w2(self.relu(self.w1(output)))
+        # return output.sum(dim=1)
+        return self.w3(output.view(x.size(0), -1))
+
+    def normalize(self):
+        # Normalize the embedding (should be realized by layernorm)
+        with torch.no_grad():
+            self.embed.weight[:] = self.embed.weight - self.embed.weight.mean(dim=1, keepdim=True) 
+            # self.embed.weight[:] = self.embed.weight / self.embed.weight.norm(dim=1, keepdim=True) * 5 
+            # self.embed.weight[:] = self.embed.weight / self.embed.weight.norm() * 5 
+
+class HierGenerator:
+    def __init__(self, tree, num_tokens_except_bg, bg_token):
+        self.tree = tree
+        self.bg_token = bg_token
+        self.num_tokens_except_bg = num_tokens_except_bg
+
+    def compute_margin_prob(self):
+        # Generate according to the tree
+        probs = torch.zeros(self.num_tokens_except_bg)
+        curr_nodes = [self.tree]
+        while len(curr_nodes) > 0:
+            subnodes = curr_nodes[0]
+            curr_nodes = curr_nodes[1:]
+
+            for prob, subnode in subnodes:
+                if isinstance(subnode, int):
+                    # It is a leave node and we put it to the set
+                    probs[subnode] += prob
+                else:
+                    # the subnode is valid and we put it to the queue
+                    curr_nodes.append([(pp*prob, rr) for pp, rr in subnode])
+        return probs
+
+    def sample(self):
+        # Generate according to the tree
+        x = torch.LongTensor(self.num_tokens_except_bg).fill_(self.bg_token)
+        curr_nodes = [self.tree]
+        while len(curr_nodes) > 0:
+            subnodes = curr_nodes[0]
+            curr_nodes = curr_nodes[1:]
+
+            for prob, subnode in subnodes:
+                if random.random() < prob:
+                    if isinstance(subnode, int):
+                        # It is a leave node and we put it to the set
+                        x[subnode] = subnode
+                    else:
+                        # the subnode is valid and we put it to the queue
+                        curr_nodes.append(subnode)
+
+        # import pdb 
+        # pdb.set_trace()
+        return x
+                     
+
+class IndepGenerator:
+    def __init__(self, p, bg_token):
+        self.p = p
+        self.bg_token = bg_token
+
+    def sample(self):
+        L = len(self.p)
+        # Just do independent sampling and get the result.  
+        x = torch.LongTensor(L).fill_(self.bg_token)
+        for j in range(L):
+            if random.random() < self.p[j]:
+                x[j] = j
+
+        return x
 
 class Dataset:
-    def __init__(self, L):
+    def __init__(self, L, args):
         # M = number of tokens
         # L = length of the seq
         self.L = L
         self.M = L + 1
 
-        # Define a binary distribution and correlation between the label and the token
-        self.probs_pos = torch.ones(self.L) * 0.55
-        self.probs_pos[0] = 0.95
-        self.probs_pos[1] = 0.95
-        self.probs_neg = torch.ones(self.L) * 0.5
+        # prob_class1 = torch.ones(self.L) * 0.55
+        # prob_class1[0] = prob_class1[1] = 0.95
+
+        # prob_class2 = torch.ones(self.L) * 0.55
+        # prob_class2[2] = prob_class2[3] = 0.95
+
+        # prob_class3 = torch.ones(self.L) * 0.55
+        # prob_class3[4] = prob_class3[5] = 0.95
+
+        # prob_neg = torch.ones(self.L) * 0.5
+
+        # # Create several classes
+        # self.probs = [prob_class1, prob_class2, prob_class3, prob_neg]
+        # self.num_class = len(self.probs)
+
+        tree = [
+            [args.p0, [(args.p1, 0), (args.p1, 1), (args.p1, 2)]], 
+            [args.p0, [(args.p1, 3), (args.p1, 4), (args.p1, 5)]], 
+            [args.p0, [(args.p1, 6), (args.p1, 7), (args.p1, 8)]], 
+        ]
+
+        bg_token = self.M - 1
+
+        hier_gen = HierGenerator(tree, self.M - 1, bg_token)
+        probs = hier_gen.compute_margin_prob()
+        log.info(f"Marginal prob: {probs}")
+
+        self.gens = []
+
+        if args.enumerate_all_classes:
+            # Create 2^n classes.
+            for i in range(2**len(tree)):
+                binary_code = f"{i:b}"
+                if len(binary_code) < len(tree):
+                    binary_code = ('0' * (len(tree) - len(binary_code))) + binary_code
+                dup_tree = []
+                for j in range(len(tree)):
+                    tj = tree[j]
+                    if binary_code[j] == '0':
+                        dup_tree.append(tj)
+                    else:
+                        dup_tree.extend([(p * tj[0], subnode) for p, subnode in tj[1]])
+
+                print(dup_tree)
+                self.gens.append(HierGenerator(dup_tree, self.M - 1, bg_token))
+
+        else:
+            # do a reduction
+            self.gens = [hier_gen]
+            for t in range(len(tree)):
+                # reduce t-th line
+                dup_tree = tree[:t] + tree[t+1:]
+                dup_tree.extend([(p * tree[t][0], subnode) for p, subnode in tree[t][1] ]) 
+                print(dup_tree)
+                self.gens.append(HierGenerator(dup_tree, self.M - 1, bg_token))
+                
+            self.gens.append(IndepGenerator(probs, bg_token))
+
+        self.num_class = len(self.gens)
+  
 
     def generate(self, batchsize):
         x = torch.LongTensor(batchsize, self.L)
         label = torch.LongTensor(batchsize)
         for i in range(batchsize):
-            if random.random() > 0.5:
-                # Positive
-                probs = self.probs_pos
-                label[i] = 1
-            else:
-                # Negative. 
-                probs = self.probs_neg
-                label[i] = 0
+            label[i] = random.randint(0, self.num_class - 1)
 
             # try each token to see whether they appear. 
-            for j in range(self.L):
-                if random.random() < probs[j]:
-                    x[i,j] = j
-                else:
-                    # Last token
-                    x[i,j] = self.M - 1
+            x[i, :]= self.gens[label[i]].sample()
 
         return x, label
     
@@ -115,27 +231,34 @@ def main(args):
     log.info(common_utils.print_info(args))
     common_utils.set_all_seeds(args.seed)
 
-    dataset = Dataset(args.L)
-    model = Model(dataset.M, dataset.L, args.d)
-    model_linear = LinearModel(args.L)
+    dataset = Dataset(args.L, args)
+    model = Model(dataset.M, dataset.L, args.d, dataset.num_class)
+    model_linear = LinearModel(args.L, dataset.num_class)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.opt.lr, momentum=args.opt.momentum, weight_decay=args.opt.wd)
+    if args.opt.method == "adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.opt.lr, weight_decay=args.opt.wd)
+    elif args.opt.method == "sgd":
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.opt.lr, momentum=args.opt.momentum, weight_decay=args.opt.wd)
+    else:
+        raise RuntimeError(f"unknown method {args.opt.method}")
+
     optimizer_linear = torch.optim.SGD(model_linear.parameters(), lr=args.opt.lr, momentum=args.opt.momentum, weight_decay=args.opt.wd)
 
-    loss_func = torch.nn.BCELoss()
+    loss_func = torch.nn.CrossEntropyLoss() 
 
     for t in range(args.niter):
         optimizer.zero_grad()
         optimizer_linear.zero_grad()
 
+        if t % 1000 == 0:
+            torch.save(dict(model=model.state_dict(), model_linear=model_linear.state_dict()), f"iter-{t}.pth")
+
         x, label = dataset.generate(args.batchsize)
         output = model(x)
         output_linear = model_linear(x)
 
-        single_output = output.squeeze().sum(dim=1)
-        loss = loss_func(F.sigmoid(single_output), label.float())
-
-        loss_linear = loss_func(F.sigmoid(output_linear), label.float())
+        loss = loss_func(output, label)
+        loss_linear = loss_func(output_linear, label)
 
         if t % 100 == 0:
             log.info(f"[{t}] loss: {loss.detach().cpu().item()}")
@@ -147,8 +270,7 @@ def main(args):
         loss_linear.backward()
         optimizer_linear.step()
 
-    #import pdb 
-    #pdb.set_trace()
+        # model.normalize()
 
     # log.info("Embedding K:")
     # log.info(model.K.weight)
